db28cd5a0eed6883553d9d131229ce6f14d9b0fd
Todd Esposito <todd@toddesposito.com>
2022-01-17 14:11:28 -0600

    Added stdout feedback to generate command

--- src/lib/generate.js
+++ src/lib/generate.js
@@ -19,24 +19,27 @@ function generate(argv = {}) {
   const config = require('../common/config').getResolved()
   const git = require('../common/git')
 
+  process.stdout.write("cvbump: Building change log...")
   const tagGroups = changelog.buildTagGroups(git.log())
 
-  const outfile = fs.createWriteStream(config.changelog)
-  outfile.on('finish', () => {
-    if (argv.commit) {
-      git.commit([config.changelog], "cvbump - (re-)generate changelog")
-      console.log(`cvbump: generated and committed ${config.changelog}`)
-    }
+  const outfile = fs.createWriteStream(config.changelog).on('finish', () => {
+      if (argv.commit) {
+        git.commit([config.changelog], "cvbump - (re-)generate changelog")
+        console.log(`cvbump: generated and committed ${config.changelog}`)
+      }
   })
+  process.stdout.write(".")
   outfile.write(config.preamble)
 
   if (tagGroups.length > 1) {
     // we have at least one version tag; We always have the '' (unreleased) tag
     for (const group of tagGroups.slice(1)) {
+      process.stdout.write(".")
       const subtree = changelog.buildChangeTree(config, group.commits)
       outfile.write(changelog.buildLogSection(config, subtree, group))
       outfile.write('\n')
     }
   }
   outfile.end(config.postamble)
+  process.stdout.write("done.\n")
 }
